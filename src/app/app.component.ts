61d7e31df61e0f5b4afe12e30a9aa69f34c99a17
Isaac Bedard <ibedard@renovo1.com>
2019-02-11 09:39:29 -0500

    Add demo to wrap console.log to run outside of Angular
    The problem can be fixed if console.log is run outside Angular. Better demo that.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private timesCalled = 0;
   private loggingEnabled = false;
+  private consoleLogWrapped = false;
+  private originalConsoleLogFunc: (args: any[]) => void;
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private zone: NgZone
+  ) {
+  }
+
+  ngOnInit() {
+    this.originalConsoleLogFunc = console.log;
+  }
 
   getTimesFunctionWasCalled() {
     if (this.loggingEnabled) {
@@ -24,5 +34,25 @@ export class AppComponent {
 
   runChangeDetection() {
     this.cdr.detectChanges();
+  }
+
+  wrapConsoleLog() {
+    if (this.consoleLogWrapped) {
+      return;
+    }
+
+    console.log = (...args) => {
+      this.zone.runOutsideAngular(() => this.originalConsoleLogFunc(args));
+    };
+    this.consoleLogWrapped = true;
+  }
+
+  unwrapConsoleLog() {
+    if (!this.consoleLogWrapped) {
+      return;
+    }
+
+    console.log = this.originalConsoleLogFunc;
+    this.consoleLogWrapped = false;
   }
 }
